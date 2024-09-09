010b47dc006738a4457c4bf12bfcecd2f103cf91
nullreff <git@nullreff.net>
2024-09-09 13:02:02 -0700

    Animation on mouse leave

--- components/home/nullreff-logo.tsx
+++ components/home/nullreff-logo.tsx
@@ -3,26 +3,39 @@ import localFont from 'next/font/local'
 const DSEG7ClassicFont = localFont({src: '../../styles/fonts/DSEG7Classic-BoldItalic-nullreff-edit.woff2' })
 
 export default function NullreffLogo() {
-    const maxDate = new Date(8640000000000000).getTime();
+    const minDate = new Date(-8640000000000000).getTime();
     const birthday = Date.parse('1990-10-02');
-    const [hoverTime, setHoverTime] = useState(maxDate);
+    const [hoverTime, setHoverTime] = useState(minDate);
+    const [hovering, setHovering] = useState(false);
     const [text, setText] = useState('nullreff');
 
     useEffect(() => {
         if (Date.now() > hoverTime + 1000) {
-            const hours = Math.abs(Date.now() - birthday) / 36e5;
-            const text = hours.toFixed().padStart(8, '0');
-            setText(text);
+            if (hovering) {
+                const hours = Math.abs(Date.now() - birthday) / 36e5;
+                setText(hours.toFixed().padStart(8, '0'));
+            } else {
+                setText('nullreff');
+            }
         } else if (Date.now() > hoverTime) {
-            const text = (Math.random() + 1).toString(36).substring(2, 10);
-            setText(text);
+            setText((Math.random() + 1).toString(36).substring(2, 10));
         } else {
-            setText('nullreff');
+            setHoverTime(Date.now());
         }
     })
 
+    function enter() {
+        setHoverTime(Date.now());
+        setHovering(true);
+    }
+
+    function leave() {
+        setHoverTime(Date.now());
+        setHovering(false);
+    }
+
     return (
-        <div onMouseEnter={() => setHoverTime(Date.now())} onMouseLeave={() => setHoverTime(maxDate)}>
+        <div onMouseEnter={enter} onMouseLeave={leave}>
             <span className={DSEG7ClassicFont.className}>{text}</span>
         </div>
     )
