8cd4d139ea2a331845d694fb5a8fa2ebc23317b9
Nullreff <git@nullreff.net>
2024-11-08 21:06:54 -0800

    Bump dependencies

--- flake.nix
+++ flake.nix
@@ -5,16 +5,9 @@
 
   outputs = { self, nixpkgs }:
     let
-      overlays = [
-        (final: prev: rec {
-          nodejs = prev.nodejs-18_x;
-          pnpm = prev.nodePackages.pnpm;
-          yarn = (prev.yarn.override { inherit nodejs; });
-        })
-      ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
+        pkgs = import nixpkgs { inherit system; };
       });
     in
     {
